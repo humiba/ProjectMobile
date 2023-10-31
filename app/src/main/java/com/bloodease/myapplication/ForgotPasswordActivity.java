0255db4ff12cd56456e8ad28559cebb045b7944c
ThVy02 <20522175@gm.uit.edu.vn>
2023-10-31 18:09:02 +0700

    add files signin-signup

---
+++ app/src/main/java/com/bloodease/myapplication/ForgotPasswordActivity.java
@@ -0,0 +1,69 @@
+package com.bloodease.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.button.MaterialButton;
+
+public class ForgotPasswordActivity extends AppCompatActivity {
+
+    private EditText emailEditText;
+    private MaterialButton resetButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgetpassword);
+
+        emailEditText = findViewById(R.id.email);
+        resetButton = findViewById(R.id.fpwbtn);
+
+        // Set a TextWatcher to monitor text changes in the EditText field
+        TextWatcher textWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                // Not needed for this example
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                // Not needed for this example
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                // Check if the email EditText is empty
+                String email = emailEditText.getText().toString().trim();
+                boolean isEmailEmpty = email.isEmpty();
+
+                // Enable or disable the reset button based on whether the email field is empty
+                resetButton.setEnabled(!isEmailEmpty);
+            }
+        };
+
+        // Set the TextWatcher for the email EditText field
+        emailEditText.addTextChangedListener(textWatcher);
+
+        // Set an OnClickListener for the reset button
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Handle the password reset process here
+                // You can send a reset link or perform the necessary actions
+                // After the reset process, you can navigate to another screen if needed
+
+                // For example, navigate to a confirmation screen
+                Intent intent = new Intent(ForgotPasswordActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
+
---
+++ app/src/main/java/com/bloodease/myapplication/SignUpActivity.java
@@ -0,0 +1,99 @@
+package com.bloodease.myapplication;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
+import android.text.method.PasswordTransformationMethod;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.UnderlineSpan;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.button.MaterialButton;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private EditText usernameEditText, emailEditText, passwordEditText, rePasswordEditText;
+    private boolean isPasswordVisible = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        usernameEditText = findViewById(R.id.username);
+        emailEditText = findViewById(R.id.email);
+        passwordEditText = findViewById(R.id.password);
+        rePasswordEditText = findViewById(R.id.repassword);
+
+        MaterialButton signUpButton = findViewById(R.id.signupbtn);
+        final TextView signInText = findViewById(R.id.signInLink);
+
+
+
+        // Show/hide password with an eye icon
+        passwordEditText.setTransformationMethod(PasswordTransformationMethod.getInstance());
+
+        passwordEditText.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.eyesoff, 0);
+
+        passwordEditText.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                int DRAWABLE_RIGHT = 2;
+                if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                    if (motionEvent.getRawX() >= (passwordEditText.getRight() - passwordEditText.getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width())) {
+                        isPasswordVisible = !isPasswordVisible;
+                        if (isPasswordVisible) {
+                            passwordEditText.setTransformationMethod(null);
+                            passwordEditText.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.eyeson, 0);
+                        } else {
+                            passwordEditText.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                            passwordEditText.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.eyesoff, 0);
+                        }
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Check if any of the EditText fields are empty
+                String username = usernameEditText.getText().toString().trim();
+                String email = emailEditText.getText().toString().trim();
+                String password = passwordEditText.getText().toString().trim();
+                String rePassword = rePasswordEditText.getText().toString().trim();
+
+                boolean isAnyFieldEmpty = username.isEmpty() || email.isEmpty() || password.isEmpty() || rePassword.isEmpty();
+
+                if (isAnyFieldEmpty) {
+                    Toast.makeText(SignUpActivity.this, "Please enter your information!", Toast.LENGTH_SHORT).show();
+                } else {
+                    // Perform the sign-up process here
+
+                    // After successful sign-up, navigate to the main screen
+                    Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        });
+
+        SpannableStringBuilder spannableStringBuilder = new SpannableStringBuilder(signInText.getText().toString());
+        SpannableString spannableString = new SpannableString("Sign in");
+        spannableString.setSpan(new ForegroundColorSpan(Color.parseColor("#FB6F78")), 0, spannableString.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spannableString.setSpan(new UnderlineSpan(), 0, spannableString.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spannableStringBuilder.replace(spannableStringBuilder.toString().indexOf("Sign in"), spannableStringBuilder.toString().indexOf("Sign in") + "Sign in".length(), spannableString);
+        signInText.setText(spannableStringBuilder);
+    }
+}
