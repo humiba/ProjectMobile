ffe834efdec6cff7e24f928e1765a4ae0223e4f6
MinhThu1987 <83085924+MinhThu1987@users.noreply.github.com>
2023-11-12 14:29:29 +0700

    MainActivity.java

--- app/src/main/java/com/bloodease/myapplication/MainActivity.java
+++ app/src/main/java/com/bloodease/myapplication/MainActivity.java
@@ -9,6 +9,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+        setContentView(R.layout.chinhsachbaomat);
+//       setContentView(R.layout.vechungtoi);
+//        setContentView(R.layout.bao1);
+//       setContentView(R.layout.bao2);
     }
 }
